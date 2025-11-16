565496374c8cf45c0cca0efb18591fb52e4e9230
금가경 <gageum0@gmail.com>
2025-11-16 17:49:05 +0900

    HomeView로 루트뷰 교체

--- Moda/App/ModaApp.swift
+++ Moda/App/ModaApp.swift
@@ -14,7 +14,7 @@ struct ModaApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $navigator.path) {
-                FollowAPITestView()
+                HomeView()
                     .navigationDestination(for: NavigationDestination.self) { destination in
                         destination.view()
                     }
