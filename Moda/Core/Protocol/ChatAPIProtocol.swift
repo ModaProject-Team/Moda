c42e9af276dd95bd3c90670a95d5eee8ccd5c921
morgan4563 <morganpark4563@gmail.com>
2025-11-24 15:13:49 +0900

    채팅탭 채팅기능을 위한, ChatAPI, ChatRouter 추가

---
+++ Moda/Core/Protocol/ChatAPIProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  ChatAPIProtocol.swift
+//  Moda
+//
+//  Created by hyunMac on 11/24/25.
+//
+
+import Foundation
+
+protocol ChatAPIProtocol {
+    func getOrCreateRoom(opponentId: String) async throws -> ChatRoomResponse
+
+    func getRooms() async throws -> ChatRoomListResponse
+
+    func sendMessage(roomId: String, content: String?, files: [String]?) async throws -> ChatMessageResponse
+
+    func getMessages(roomId: String, cursorDate: String?) async throws -> ChatHistoryResponse
+}
